80c19bc7812ff98010e93b7305af334a64faa1a3
최종호 <victor_77@naver.com>
2020-07-03 17:35:10 +0900

    add package, sass and router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "../style/test.scss";
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>hi</div>;
 }
 
 export default App;
